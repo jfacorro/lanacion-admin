f4e50daa00609894b7f46a575740bab80625915f
amilkr <amilcar.almonacid@gmail.com>
2015-04-11 07:47:27 -0300

    no belongs_to

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -7,7 +7,6 @@ class Promo < ActiveRecord::Base
 
   validates :lanacionid, uniqueness: true
 
-  belongs_to :business
   belongs_to :category
 
   scope :active, -> { where('? BETWEEN date_from AND date_to', Time.now) }
