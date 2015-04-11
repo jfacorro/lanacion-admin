4b6a25693ae4906540cf921259a4c5e64b479382
amilkr <amilcar.almonacid@gmail.com>
2015-04-11 01:18:18 -0300

    little changes over the controller. updated format promo method (we should use a serialyzer, but you know (shrug))

--- app/controllers/api_promos_controller.rb
+++ app/controllers/api_promos_controller.rb
@@ -1,47 +1,49 @@
 class ApiPromosController < ApplicationController
   def find_by_id
-    promo = Promo.where('id = ?', params[:id]).first
+    promo = Promo.find(params[:id])
     render json: format_promo(promo)
   end
 
   def find_by_category
-    category = Category.where('name = ?', params[:categoria]).first
-    promos = Promo.where('category_id = ? AND ? BETWEEN date_from AND date_to', category.id, Time.now)
+    category = Category.where(name: params[:categoria]).first
+    promos = category.promos #.active
     render json: promos.map { |p| format_promo(p) }
   end
 
   def find_by_location
-    distance = (params[:distancia] == nil ? params[:distancia] : 200) / 1000
+    distance = (params[:distancia].nil? ? params[:distancia] : 200) / 1000
     lat = params[:latitud].to_f
     lng = params[:longitud].to_f
 
     delta_lat = (distance / 6378) * (180 / Math::PI);
     delta_lng = (distance / 6378) * (180 / Math::PI) / Math.cos(lat * Math::PI / 180);
 
-    promos = Promo.joins(:business).where('(? BETWEEN date_from AND date_to) AND (location_lat BETWEEN ? AND ?) AND (location_lng BETWEEN ? AND ?)', Time.now, lat - delta_lat, lat + delta_lat, lng - delta_lng, lng + delta_lng)
+    promos = Promo.joins(:business)
+      .where('(location_lat BETWEEN ? AND ?) AND (location_lng BETWEEN ? AND ?)', lat - delta_lat, lat + delta_lat, lng - delta_lng, lng + delta_lng)
+      #.active
     render json: promos.map { |p| format_promo(p) }
   end
 
   private
   def format_promo(promo)
     {
-      :_id => promo.id,
-      :id => promo.id,
+      :_id => promo.lanacionid,
+      :id => promo.id.to_s,
       :point => [promo.business.location_lng,
                  promo.business.location_lat],
-      :imagen => promo.business.image,
-      :hasta => promo.date_from,
-      :desde => promo.date_to,
+      :imagen => promo.image,
+      :desde => promo.date_from,
+      :hasta => promo.date_to,
       :establecimiento => {:id => promo.business.id,
                            :nombre => promo.business.name,
                            :sucursal => promo.business.branch,
                            :direccion => promo.business.address},
       :beneficio => {:id => promo.id,
-                     :tipo => promo.promo_type.name,
+                     :tipo => promo.ptype,
                      :nombre => promo.business.name,
-                     :description => promo.description,
+                     :descripcion => promo.description,
                      :categoria => promo.category.name,
-                     :subcategoria => promo.subcategory.name,
+                     :subcategoria => promo.subcategory,
                      :tarjeta => promo.card}
     }
   end
